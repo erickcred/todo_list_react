b37b8c11c043a4c2bba5e867c7bfd989cec55995
Erick Rick <erickcredjes@gmail.com>
2023-04-10 18:00:00 -0300

    Stilizando Barralateral e componetes

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,5 +1,23 @@
+import FiltroCard from '../../components/FiltroCard/index'
+import * as S from './styles'
+
 const BarraLateral = () => {
-  return <aside>Barra Lateral</aside>
+  return (
+    <>
+      <S.Aside>
+        <S.Campo type="text" placeholder="Burcar" />
+
+        <S.Filtros>
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard />
+          <FiltroCard ativo={true} />
+        </S.Filtros>
+      </S.Aside>
+    </>
+  )
 }
 
 export default BarraLateral
---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  background-color: #eeeeee;
+  padding: 16px;
+  height: 100vh;
+`
+
+export const Campo = styled.input`
+  background-color: #fff;
+  border-radius: 8px;
+  border-color: #666666;
+  font-weight: bold;
+  padding: 8px;
+  width: 100%;
+`
+
+export const Filtros = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 8px;
+  margin-top: 14px;
+`
